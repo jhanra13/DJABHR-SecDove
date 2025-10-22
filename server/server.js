469802cec8c40091d5314207ff19ea4a91238790
gyori <gyoridylan@gmail.com>
2025-10-21 22:30:04 -0700

    CORS Issue fix test3

--- server/server.js
+++ server/server.js
@@ -90,6 +90,7 @@ app.use(helmet({
 }));
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
