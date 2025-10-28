4e3103e498aca6f8076cd0f9a268d85c25493de6
Dylan <dylan.gyori@wsu.edu>
2025-10-28 00:22:24 -0700

    Added Anthony's scripts

---
+++ server/config/jest.config.js
@@ -0,0 +1,16 @@
+// export default {
+//   transform: {},
+//   extensionsToTreatAsEsm: ['.js'],
+// };
+
+// export default {
+//   testEnvironment: 'node',
+//   transform: {}, // disables Babel transform
+// };
+
+// jest.config.js
+export default {
+  testEnvironment: 'node',
+  transform: {}, // disable Babel
+  extensionsToTreatAsEsm: ['.js'],
+};
